855faac437291ee5069bd3e3df14c5c6166638fc
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-09 14:22:33 +0900

    up

---
+++ Arbitrary_side/main.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream ofs;
+
+    ofs.open("file.txt");
+
+    ofs.write("This is an apple", 16);
+
+    long pos = ofs.tellp();
+
+    ofs.seekp(pos-7);
+    ofs.write(" sam", 4);
+    ofs.close();
+
+    return 0;
+}
