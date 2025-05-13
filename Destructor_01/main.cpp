d29869a99b281daff22de9a6c2bc7eb4f10bfde4
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-13 10:55:58 +0900

    ãtuesday_1

---
+++ Destructor_01/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Person {
+private:
+    char* name;
+    int age;
+public:
+    Person(char *myname, int myage) {
+        int len=strlen(myname)+1;
+        name=new char[len];
+        strcpy(name, myname);
+        age=myage;
+    }
+    void ShowPersonInfo() const {
+        cout<<"이름: "<<name<<endl;
+        cout<<"나이: "<<age<<endl;
+    }
+    ~Person() {
+        delete []name;
+        cout<<"called destructor!"<<endl;
+    }
+};
