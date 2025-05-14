951dd681c0559869faa776ebb732ee5699d658f4
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-14 16:58:16 +0900

    wednesday_7

--- Type_returnOp_Overload/main.cpp
+++ Type_returnOp_Overload/main.cpp
@@ -14,9 +14,18 @@ public:
         num=ref.num;
         return *this;
     }
-    operator int() {
-        return num;
+/*
+    Number& operator+(int n) {
+        cout<<"operator+()"<<endl;
+        num+=n;
+        return *this;
     }
+*/
+    operator int() { // <- int는 함수이름으로 못씀 하지만 이렇게 쓰면 형 변환 연산자 기능이 됨
+        cout<<"int()"<<endl;
+        return num; // 현 변환도 반환 타입이 없다.
+    }
+
     void ShowNumber() {
         cout<<num<<endl;
     }
@@ -25,7 +34,14 @@ public:
 int main(void) {
     Number num1;
     num1=30;
+
     Number num2=num1+20;
     num2.ShowNumber();
+
+//    int n = num2;
+    cout<<"type cast : "<<num2<<endl; // (묵시적 형변환) // 객체는 consolout 못씀
+
     return 0;
 }
+
+// 복사 생성자 출력과정 만들어 보기 (복사 생성자 코드는 입력되지 않아서 호출이 되지 않았다.)
