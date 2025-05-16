a9fbb35e85af1fa235ac85389774f5947b0c1809
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-16 16:55:48 +0900

    friday_6

---
+++ Lambda_Sort/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm> // Sort 함수
+#include <vector>
+
+using namespace std;
+
+class Multiplier {
+    int first;
+    int second;
+
+public:
+Multiplier(int f, int s) {
+    first=f;
+    second=s;
+    }
+
+int getResult() {
+    return first* second;
+    }
+};
+int main() {
+    vector<Multiplier> multipleData;
+
+    for(int i=0; i<10; i++) {
+        Multiplier multiple = Multiplier(rand() % 10, rand() % 10);
+        multipleData.push_back(multiple);
+    }
+    cout<<"정렬되지 않은 결과"<<endl;
+    for(vector<Multiplier>::iterator iter = multipleData.begin();
+         iter != multipleData.end(); iter++) {
+        cout<<iter->getResult()<<endl;
+    }
+    sort(multipleData.begin(), multipleData.end(), [](Multiplier& m1, Multiplier& m2){
+        return m1.getResult()<m2.getResult();
+    }); // 비교함수를 람다 형식으로 구현
+    cout<<"정렬된 결과"<<endl;
+    for(vector<Multiplier>::iterator iter=multipleData.begin();
+         iter != multipleData.end(); iter++) {
+        cout<<iter->getResult()<<endl;
+    }
+    return 0;
+}
