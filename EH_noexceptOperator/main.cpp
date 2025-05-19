1bb2a6deccec2a00c806a3e1e9172f0b945d7039
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-19 12:37:25 +0900

    monday_7

---
+++ EH_noexceptOperator/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void handler() {
+    cout<<"in handler"<<endl;
+}
+
+void f1(void) noexcept(false) {         // throw(int)
+    cout<<"About to throw 1"<<endl;
+    if (1)
+        throw 1;
+}
+
+void f2(void) noexcept {                // throw()
+    try {
+        f1();
+    }
+    catch(...){
+        handler();
+//        throw;
+    }
+}
+int main() {
+//    f1();
+
+    try {
+        f2();
+    }
+    catch(...) {
+        cout<<"Caught exception from f2"<<endl;
+    }
+}
