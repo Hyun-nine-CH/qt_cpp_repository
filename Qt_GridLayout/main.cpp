4d2cfb4c43319cfa3e18ee7d386ed73f9c7f76e2
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-21 09:51:16 +0900

    wednesday_2

--- Qt_GridLayout/main.cpp
+++ Qt_GridLayout/main.cpp
@@ -1,9 +1,11 @@
+#include "widget.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-   QApplication a(argc, argv);
-
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
     return a.exec();
 }
