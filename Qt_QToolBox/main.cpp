bbd0db71938d50591784af1e92afc753c5eed483
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-21 11:32:57 +0900

    wednesday_4

---
+++ Qt_QToolBox/main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include <QToolBox>
+#include <QDial>
+#include <QCalendarWidget>
+#include <QTextEdit>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QToolBox toolbox;
+    toolbox.resize(300,380);
+    toolbox.show();
+
+    QDial dial(&toolbox);
+    QCalendarWidget calendarWidget(&toolbox);
+    QTextEdit textEdit("QTextEdit", &toolbox);
+    toolbox.addItem(&dial, "Page &1");
+    toolbox.addItem(&calendarWidget, "Page &2");
+    toolbox.addItem(&textEdit, "Page &3");
+
+    return a.exec();
+}
