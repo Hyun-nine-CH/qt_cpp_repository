0f53784c083310cd0d5ac384c7a629d4d3dfd736
Hyun-nine-CH <ttmstym234@gmail.com>
2025-05-25 12:13:30 +0900

    Sunday_2

--- Qt_Calc/widget.cpp
+++ Qt_Calc/widget.cpp
@@ -4,8 +4,9 @@ Widget::Widget(QWidget *parent)
     : QWidget(parent), m_isFirst(true) {
     m_label=new QLabel("0", this);
     m_label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-//    m_label->setFrameShape(QFrame::Box);
-    m_label->setGeometry(10,5,230,40);
+    m_label->setFrameShape(QFrame::Box);
+    m_label->setMaximumHeight(30);
+//    m_label->setGeometry(10,5,230,40);
 
     const char ButtonChar[16][2]={
         "7","8","9","/",
@@ -14,12 +15,28 @@ Widget::Widget(QWidget *parent)
         "0","C","=","+"
     };
 
+    QGridLayout*gridLayout=new QGridLayout();
+    buttonGroup=new QButtonGroup(this);
+    connect(buttonGroup, SIGNAL(idClicked(int)),SLOT(click(int)));
+    for(int y=0; y<WIDTH; ++y) {
+        for(int x=0; x<WIDTH; ++x) {
+            int n = x+y*WIDTH;
+            buttonGroup->addButton(new QPushButton(ButtonChar[n],this),n);
+            gridLayout->addWidget(buttonGroup->button(n),n/WIDTH,n%WIDTH);
+            //m_buttons.append(new QPushButton(ButtonChar[n],this));
+            //gridLayout->addWidget(m_buttons.at(n),n/WIDTH,n%WIDTH);
+            //if(n!=13&&n!=14)
+            //    connect(m_buttons.at(n),SIGNAL(clicked()),methods[n]);
+        }
+    }
+/*
     for(int y=0; y<WIDTH; ++y) {
         for(int x=0; x< WIDTH; ++x) {
             m_buttons.append(new QPushButton(ButtonChar[x+y*WIDTH],this));
             m_buttons.at(x+y*WIDTH)->setGeometry(5+60*x,50+60*y,60,60);
         }
     }
+*/
     connect(m_buttons.at(0), SIGNAL(clicked()),SLOT(setNum()));
     connect(m_buttons.at(1), SIGNAL(clicked()),SLOT(setNum()));
     connect(m_buttons.at(2), SIGNAL(clicked()),SLOT(setNum()));
@@ -57,15 +74,47 @@ Widget::Widget(QWidget *parent)
         m_label->setText(QString::number(result));
     });
 
-    setMinimumSize(250,295);
-    setMaximumSize(250,295);
+//    setMinimumSize(250,295);
+//    setMaximumSize(250,295);
     setWindowTitle("Calculater");
+    QBoxLayout*vBoxLayout=new QVBoxLayout(this);
+    vBoxLayout->setContentsMargins(6,6,6,6);
+    vBoxLayout->addWidget(m_label);
+    vBoxLayout->addLayout(gridLayout);
+    setLayout(vBoxLayout);
 }
 
 Widget::~Widget() {
     delete m_label;
-    m_buttons.clear();
+    Q_FOREACH(auto b, buttonGroup->buttons()) delete b;
+    //    m_buttons.clear();
 }
+void Widget::click(int id) {
+    QPushButton *button=(QPushButton*)((QButtonGroup*)sender())->button(id);
+    QString bStr;
+    if(button!=nullptr) bStr=button->text();
+    switch(id) {
+    case 0: case 1: case 2:
+    case 4: case 5: case 6:
+    case 8: case 9: case 10:
+    case 12:
+        if(m_label!=nullptr) {
+            QString IStr=m_label->text();
+            m_label->setText((IStr=="0"|m_isFirst)?bStr:IStr+bStr);
+            m_isFirst=false;
+        }
+        break;
+
+    case 3: case 7: case 11: case 15:
+        if(m_label!=nullptr)
+            m_num1=m_label->text();
+        m_op=bStr;
+        m_isFirst=true;
+        break;
+    case 13: case 14: break;
+    };
+}
+/*
 void Widget::setNum() {
     QPushButton *button=dynamic_cast<QPushButton*>(sender());
     QString bStr;
@@ -84,7 +133,7 @@ void Widget::setOp() {
         m_isFirst=true;
     }
 }
-
+*/
 // updated info.
 /*
 switch(m_op.at(0).toLatin1()) {
--- Qt_Calc/widget.h
+++ Qt_Calc/widget.h
@@ -5,30 +5,37 @@
 #include <QGridLayout>
 
 #include <QWidget>
-#include <QVector>
+//#include <QVector>
 #include <QLabel>
 #include <QPushButton>
+#include <QButtonGroup>
 
 class QLabel;
 class QPushButton;
+class QButtonGroup;
 
 class Widget : public QWidget
 {
     Q_OBJECT
+private:
+    QLabel*m_label;
+    QButtonGroup*buttonGroup;
+    QString m_num1, m_op;
 
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
 private:
-    QLabel *m_label;
+//    QLabel *m_label;
     QVector<QPushButton*> m_buttons;
-    QString m_num1, m_op;
+//    QString m_num1, m_op;
     bool m_isFirst;
     const qint32 WIDTH = 4;
 
 public slots:
-    void setNum();
-    void setOp();
+//    void setNum();
+//    void setOp();
+    void click(int id);
 };
 #endif // WIDGET_H
