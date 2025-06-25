745206fedbf4b71013a3773f99637bd0a2fd2990
Hyun-nine-CH <ttmstym234@gmail.com>
2025-06-25 14:12:35 +0900

    Wednesday+3

--- PainterApp/widget.cpp
+++ PainterApp/widget.cpp
@@ -1,9 +1,17 @@
 #include "widget.h"
+#include <QPainter>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-{}
+{
+    resize(200,200);
+}
 
 Widget::~Widget() {}
 
-void Widget::paintEvent(QPaintEvent *event) { }
+void Widget::paintEvent(QPaintEvent *event) {
+    QPainter *painter=new QPainter(this);
+    painter->setPen(QPen(Qt::blue, 4, Qt::SolidLine));
+    painter->drawChord(20,20,160,160,30*16,130*16);
+    delete painter;
+}
--- PainterApp/widget.h
+++ PainterApp/widget.h
@@ -14,6 +14,5 @@ public:
 protected:
     void paintEvent(QPaintEvent *event)
 override;
-
 };
 #endif // WIDGET_H
