2c507239d0aa613c32271e3767d7d829baa4dbe9
Hyun-nine-CH <ttmstym234@gmail.com>
2025-06-25 15:46:05 +0900

    Wednesday+4

--- PainterApp/widget.cpp
+++ PainterApp/widget.cpp
@@ -1,5 +1,7 @@
 #include "widget.h"
 #include <QPainter>
+#include <QPen>
+#include <QFont>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -11,7 +13,19 @@ Widget::~Widget() {}
 
 void Widget::paintEvent(QPaintEvent *event) {
     QPainter *painter=new QPainter(this);
-    painter->setPen(QPen(Qt::blue, 4, Qt::SolidLine));
-    painter->drawChord(20,20,160,160,30*16,130*16);
+//    painter->setPen(QPen(Qt::blue, 4, Qt::SolidLine));
+//    painter->drawChord(20,20,160,160,30*16,130*16);
+
+    QPen pen=painter->pen();
+    pen.setStyle(Qt::DotLine);
+    painter->setPen(pen);
+    painter->drawText(20,20,"Hello");
+
+    QFont font=painter->font();
+    font.setPixelSize(48);
+    painter->setFont(font);
+    painter->drawText(rect(),Qt::AlignCenter,tr("Qt"));
+
     delete painter;
+
 }
