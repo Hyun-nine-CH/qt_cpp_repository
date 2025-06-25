53de125ec803a2a40cd183cec414e89bacd627d3
Hyun-nine-CH <ttmstym234@gmail.com>
2025-06-25 17:21:29 +0900

    Wednesday+5

---
+++ SVG_file/widget.cpp
@@ -0,0 +1,25 @@
+#include <QSvgRenderer>
+#include <QPainter>
+
+#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    resize(300,300);
+    buffer=QImage(size(), QImage::Format_ARGB32_Premultiplied);
+    QPainter painter(&buffer);
+    painter.setViewport(0,0,width(),height());
+    painter.eraseRect(0,0,width(),height());
+
+    QString filename("sample.svg");
+    QSvgRenderer svgr(filename);
+    svgr.render(&painter);
+}
+
+Widget::~Widget() {}
+
+void Widget::paintEvent(QPaintEvent*) {
+    QPainter painter(this);
+    painter.drawImage(0,0,buffer);
+}
