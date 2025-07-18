efc534d66b33946ee179da338f09f4bcfe559531
Hyun-nine-CH <ttmstym234@gmail.com>
2025-07-18 14:42:46 +0900

    Friday_4

---
+++ Cat/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include "cat.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Cat cat;
+    QTest::qExec(&cat);
+
+    return a.exec();
+}
